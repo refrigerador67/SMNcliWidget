d9f7c8f76e6b7b1f31311c21ef3b339944c411bb
Nix <75538775+nixietab@users.noreply.github.com>
2025-11-04 21:41:44 -0300

    Update SMNWeather to use new weather API
    Refactor SMNWeather class to support new API endpoints

--- app/src/main/java/com/nixietab/smncliwork/SMNWeather.java
+++ app/src/main/java/com/nixietab/smncliwork/SMNWeather.java
@@ -14,61 +14,57 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
-
 public class SMNWeather {
 
-    private static final String API_BASE = "https://ws.smn.gob.ar/map_items/weather";
-    // Returns a string list of all the locations
-    public static List<String> getAllLocations() throws IOException, JSONException {
-        JSONArray data = fetchWeatherData();
-        List<String> locations = new ArrayList<>();
-        for (int i = 0; i < data.length(); i++) {
-            JSONObject item = data.getJSONObject(i);
-            if (item.has("name")) {
-                locations.add(item.getString("name"));
-            }
-        }
-        Collections.sort(locations);
-        return locations;
-    }
-    // asks for a string of a city name ej "Capital Federal" and returns climate info in structure
-    public static JSONObject getWeatherForLocation(String cityName) throws IOException, JSONException {
-        JSONArray data = fetchWeatherData();
-        for (int i = 0; i < data.length(); i++) {
-            JSONObject item = data.getJSONObject(i);
-            if (item.has("name") && item.getString("name").toLowerCase().contains(cityName.toLowerCase())) {
-                return item;
-            }
-        }
-        return null;
-    }
+    private static final String API_BASE_LOCATIONS = "https://ws.smn.gob.ar/map_items/weather";
+    private static final String API_BASE_WEATHER = "https://ws1.smn.gob.ar/v1/weather/location/";
 
-    public static Map<String, String> getWeatherInfo(String cityName) throws IOException, JSONException {
-        JSONObject w = getWeatherForLocation(cityName);
-        if (w == null) return null;
-
-        Map<String, String> info = new HashMap<>();
-        info.put("name", w.optString("name", "Unknown"));
-
-        JSONObject weather = w.optJSONObject("weather");
-        if (weather == null) weather = new JSONObject();
-
-        info.put("description", capitalize(weather.optString("description", "N/A")));
-        info.put("temp", weather.optString("temp", "N/A"));
-        info.put("humidity", weather.optString("humidity", "N/A"));
-        info.put("pressure", weather.optString("pressure", "N/A"));
-        info.put("wind_speed", weather.optString("wind_speed", "N/A"));
-        info.put("wind_dir", weather.optString("wind_dir", ""));
-        return info;
-    }
-
-    private static JSONArray fetchWeatherData() throws IOException, JSONException {
-        handleSSLHandshake(); // Blasphemy
-        URL url = new URL(API_BASE);
+    // this gets the list of IDs for the locations using the old api thats stuck in 2022
+    public static Map<String, Integer> getAllLocationsWithIds(String jwtToken) throws IOException, JSONException {
+        handleSSLHandshake();
+        URL url = new URL(API_BASE_LOCATIONS);
         HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
         conn.setConnectTimeout(10000);
         conn.setReadTimeout(10000);
         conn.setRequestMethod("GET");
+
+        if (jwtToken != null && !jwtToken.isEmpty()) {
+            conn.setRequestProperty("Authorization", "JWT " + jwtToken);
+        }
+
+        Map<String, Integer> locations = new HashMap<>();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+            JSONArray data = new JSONArray(response.toString());
+            for (int i = 0; i < data.length(); i++) {
+                JSONObject item = data.getJSONObject(i);
+                String name = item.optString("name", null);
+                int id = item.optInt("id", -1);
+                if (name != null && id != -1) {
+                    locations.put(name, id);
+                }
+            }
+        }
+
+        return locations;
+    }
+
+    // actually fetch the data
+    public static JSONObject fetchWeatherData(int locationId, String jwtToken) throws IOException, JSONException {
+        handleSSLHandshake();
+        URL url = new URL(API_BASE_WEATHER + locationId);
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        conn.setConnectTimeout(10000);
+        conn.setReadTimeout(10000);
+        conn.setRequestMethod("GET");
+
+        if (jwtToken != null && !jwtToken.isEmpty()) {
+            conn.setRequestProperty("Authorization", "JWT " + jwtToken);
+        }
 
         try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
             StringBuilder response = new StringBuilder();
@@ -76,35 +72,51 @@ public class SMNWeather {
             while ((line = in.readLine()) != null) {
                 response.append(line);
             }
-            return new JSONArray(response.toString());
+            return new JSONObject(response.toString());
         }
+    }
+
+    // returns the stuff based on the location ID
+    public static Map<String, String> getWeatherInfoByLocationId(int locationId, String jwtToken) throws IOException, JSONException {
+        JSONObject w = fetchWeatherData(locationId, jwtToken);
+        if (w == null) return null;
+
+        Map<String, String> info = new HashMap<>();
+        JSONObject location = w.optJSONObject("location");
+        info.put("name", location != null ? location.optString("name", "Unknown") : "Unknown");
+
+        JSONObject weather = w.optJSONObject("weather");
+        info.put("description", weather != null ? capitalize(weather.optString("description", "N/A")) : "N/A");
+
+        info.put("temp", String.valueOf(w.optDouble("temperature", Double.NaN)));
+        info.put("humidity", String.valueOf(w.optDouble("humidity", Double.NaN)));
+        info.put("pressure", String.valueOf(w.optDouble("pressure", Double.NaN)));
+
+        JSONObject wind = w.optJSONObject("wind");
+        info.put("wind_speed", wind != null ? String.valueOf(wind.optDouble("speed", Double.NaN)) : "N/A");
+        info.put("wind_dir", wind != null ? wind.optString("direction", "") : "");
+
+        return info;
     }
 
     private static String capitalize(String str) {
         if (str == null || str.isEmpty()) return str;
         return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
-    public static void handleSSLHandshake() { // Please don't ever do this
-        try { // All faith is lost
+
+    public static void handleSSLHandshake() { // mmm yeah tasty oh yeas tasty code
+        try { 
             TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
-                public X509Certificate[] getAcceptedIssuers() {
-                    return new X509Certificate[0];
-                }
-
-                @Override
-                public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                }
-
-                @Override
-                public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                }
+                public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {}
             }};
 
             SSLContext sc = SSLContext.getInstance("SSL");
             sc.init(null, trustAllCerts, new SecureRandom());
             HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
             HttpsURLConnection.setDefaultHostnameVerifier((arg0, arg1) -> true);
-        } catch (Exception ignored) { // And there's how security doesn't work anymore C:
-        }
+        } catch (Exception ignored) {}
+        // no but really dont do this in production
     }
 }
